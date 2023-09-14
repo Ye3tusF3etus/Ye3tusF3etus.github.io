42de8300a41d048c8e4cce28959106d1661482a5
Ye3tusF3etus <marcojorge2004@hotmail.com>
2023-09-14 22:41:25 +0200

    Quick Update
    - Checking if php works on other servers

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+function showHint(str) {
+	if (str.length == 0) {
+	  document.getElementById("txtHint").innerHTML = "";
+	  return;
+	} else {
+	  const xmlhttp = new XMLHttpRequest();
+	  xmlhttp.onload = function() {
+		console.log(this.statusText);
+		document.getElementById("txtHint").innerHTML = this.responseText;
+	  }
+	xmlhttp.open("GET", "/public/hello.php?q=" + str);
+	xmlhttp.send();
+	}
+  }
